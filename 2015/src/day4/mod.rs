79e3a089372e723c636736f7dffdc9c492c59f45
Christer Jensen <chr.code@gmail.com>
2022-11-12 23:24:33 +0100

    Add solution 2015 - Day 4

---
+++ 2015/src/day4/mod.rs
@@ -0,0 +1,47 @@
+pub const SOLUTION: common::Solution = common::Solution {
+    name: "Day 4: The Ideal Stocking Stuffer",
+    input: std::include_bytes!("input"),
+    solve: self::solve,
+};
+
+pub fn solve(input: &[u8]) -> (String, String) {
+    let input = String::from_utf8_lossy(input).into_owned();
+    let mut hash_input = input.clone();
+    let is_zero_bits = |byte: u8, mask: u8| (byte & mask) == 0;
+
+    let mut i: u32 = 0;
+
+    // Part 1.
+    let five_zeroes: u32 = loop {
+        hash_input.push_str(&i.to_string());
+        let digest = md5::compute(hash_input.as_bytes());
+
+        let cond = is_zero_bits(digest[0], 0xff)
+            && is_zero_bits(digest[1], 0xff)
+            && is_zero_bits(digest[2], 0xf0);
+        if cond {
+            break i;
+        }
+
+        i += 1;
+        hash_input.clone_from(&input);
+    };
+
+    // Part 2.
+    let six_zeroes: u32 = loop {
+        hash_input.push_str(&i.to_string());
+        let digest = md5::compute(hash_input.as_bytes());
+
+        let cond = is_zero_bits(digest[0], 0xff)
+            && is_zero_bits(digest[1], 0xff)
+            && is_zero_bits(digest[2], 0xff);
+        if cond {
+            break i;
+        }
+
+        i += 1;
+        hash_input.clone_from(&input);
+    };
+
+    (five_zeroes.to_string(), six_zeroes.to_string())
+}
--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -1,5 +1,11 @@
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
-pub const SOLUTIONS: &[common::Solution] = &[day1::SOLUTION, day2::SOLUTION, day3::SOLUTION];
+pub const SOLUTIONS: &[common::Solution] = &[
+    day1::SOLUTION,
+    day2::SOLUTION,
+    day3::SOLUTION,
+    day4::SOLUTION,
+];
