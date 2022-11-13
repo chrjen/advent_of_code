cb794c35b348946f743dc7eaee935a8ebec50a92
Christer Jensen <chr.code@gmail.com>
2022-11-14 00:49:08 +0100

    Refactor solution for 2015 - Day 6
    Changed to parse instructions only once and save result as
    Instruction enum values. Also moved grip loop into a closure taking
    another closure for the actual mutation of each cell value.

--- 2015/src/day6/mod.rs
+++ 2015/src/day6/mod.rs
@@ -6,48 +6,73 @@ pub const SOLUTION: common::Solution = common::Solution {
     solve: self::solve,
 };
 
+type Cord = (usize, usize, usize, usize);
+
+enum Instruction {
+    TurnOn(Cord),
+    TurnOff(Cord),
+    Toggle(Cord),
+}
+
 pub fn solve(input: &[u8]) -> (String, String) {
     const GRID_SIZE: usize = 1_000_000;
     let input = String::from_utf8_lossy(input);
     let mut grid: Vec<u8> = vec![0; GRID_SIZE];
 
-    let parse = |s: &str| -> (usize, usize, usize, usize) {
-        let reg = Regex::new(r"(\d+),(\d+) through (\d+),(\d+)").unwrap();
-        let cap = reg.captures(s).unwrap();
+    // Parse all instructions.
+    let instructions = {
+        let mut result = Vec::new();
 
-        (
-            cap[1].parse().unwrap(),
-            cap[2].parse().unwrap(),
-            cap[3].parse().unwrap(),
-            cap[4].parse().unwrap(),
-        )
+        let parse = |s: &str| -> Cord {
+            let reg = Regex::new(r"(\d+),(\d+) through (\d+),(\d+)").unwrap();
+            let cap = reg.captures(s).unwrap();
+
+            (
+                cap[1].parse().unwrap(),
+                cap[2].parse().unwrap(),
+                cap[3].parse().unwrap(),
+                cap[4].parse().unwrap(),
+            )
+        };
+
+        for line in input.lines() {
+            if line.starts_with("turn on") {
+                result.push(Instruction::TurnOn(parse(&line[8..])));
+            } else if line.starts_with("turn off") {
+                result.push(Instruction::TurnOff(parse(&line[9..])));
+            } else if line.starts_with("toggle") {
+                result.push(Instruction::Toggle(parse(&line[7..])));
+            } else {
+                panic!("unknown command");
+            }
+        }
+
+        result
+    };
+
+    // Loop over subgrid specified by `cord` modifying each grid cell.
+    let map_grid = |grid: &mut Vec<u8>, cord: &Cord, f: &mut dyn FnMut(&mut u8)| {
+        let (x0, y0, x1, y1) = *cord;
+        for x in x0..=x1 {
+            for y in y0..=y1 {
+                f(&mut grid[y * 1000 + x]);
+            }
+        }
     };
 
     // Part 1
-    for line in input.lines() {
-        if line.starts_with("turn on") {
-            let (x0, y0, x1, y1) = parse(&line[8..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    grid[y * 1000 + x] = 1;
-                }
+    for instruction in instructions.iter() {
+        use Instruction::*;
+        match instruction {
+            TurnOn(cord) => {
+                map_grid(&mut grid, cord, &mut |x| *x = 1);
             }
-        } else if line.starts_with("turn off") {
-            let (x0, y0, x1, y1) = parse(&line[9..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    grid[y * 1000 + x] = 0;
-                }
+            TurnOff(cord) => {
+                map_grid(&mut grid, cord, &mut |x| *x = 0);
             }
-        } else if line.starts_with("toggle") {
-            let (x0, y0, x1, y1) = parse(&line[7..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    grid[y * 1000 + x] = 1 - grid[y * 1000 + x];
-                }
+            Toggle(cord) => {
+                map_grid(&mut grid, cord, &mut |x| *x = 1 - *x);
             }
-        } else {
-            panic!("unknown command");
         }
     }
     let sum1: u32 = grid.iter().map(|x| *x as u32).sum();
@@ -56,36 +81,26 @@ pub fn solve(input: &[u8]) -> (String, String) {
     for cell in grid.iter_mut().take(GRID_SIZE) {
         *cell = 0;
     }
-
-    for line in input.lines() {
-        if line.starts_with("turn on") {
-            let (x0, y0, x1, y1) = parse(&line[8..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    grid[y * 1000 + x] += 1;
-                }
+    for instruction in instructions.iter() {
+        use Instruction::*;
+        match instruction {
+            TurnOn(cord) => {
+                map_grid(&mut grid, cord, &mut |x| *x += 1);
             }
-        } else if line.starts_with("turn off") {
-            let (x0, y0, x1, y1) = parse(&line[9..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    if grid[y * 1000 + x] == 0 {
-                        continue;
+            TurnOff(cord) => {
+                map_grid(&mut grid, cord, &mut |x| {
+                    if *x == 0 {
+                        return;
                     }
-                    grid[y * 1000 + x] -= 1;
-                }
+                    *x -= 1;
+                });
             }
-        } else if line.starts_with("toggle") {
-            let (x0, y0, x1, y1) = parse(&line[7..]);
-            for x in x0..=x1 {
-                for y in y0..=y1 {
-                    grid[y * 1000 + x] += 2;
-                }
+            Toggle(cord) => {
+                map_grid(&mut grid, cord, &mut |x| *x += 2);
             }
-        } else {
-            panic!("unknown command");
         }
     }
+
     let sum2: u32 = grid.iter().map(|x| *x as u32).sum();
 
     (sum1.to_string(), sum2.to_string())
