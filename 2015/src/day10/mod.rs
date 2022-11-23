9c3afe7322208ccab5596151c69408fe8be9387a
Christer Jensen <chr.code@gmail.com>
2022-11-23 12:08:03 +0100

    Add solution for 2015 - Day 10

---
+++ 2015/src/day10/mod.rs
@@ -0,0 +1,86 @@
+pub const SOLUTION: common::Solution = common::Solution {
+    name: "Day 10: Elves Look, Elves Say",
+    input: std::include_bytes!("input"),
+    solve: self::solve,
+};
+
+/// Creates the look-and-say string appending it to the `dst` string.
+/// Returns an immutable reference to the `dst` string.
+fn look_and_say<'a>(dst: &'a mut String, src: &str) -> &'a str {
+    let mut chars = src.chars();
+    let Some(mut prev) = chars.next() else {
+            return dst;
+        };
+    let mut count: u32 = 1;
+
+    for ch in chars {
+        if prev == ch {
+            count += 1;
+            continue;
+        }
+
+        dst.push_str(&format!("{}{}", count, prev));
+
+        prev = ch;
+        count = 1;
+    }
+
+    dst.push_str(&format!("{}{}", count, prev));
+
+    dst
+}
+
+pub fn solve(input: &[u8]) -> (String, String) {
+    let input = String::from_utf8_lossy(input);
+
+    let src = &mut input.into_owned();
+    let dst = &mut String::new();
+
+    for _ in 0..40 {
+        dst.clear();
+        look_and_say(dst, src);
+        std::mem::swap(dst, src);
+    }
+    let part1 = src.len();
+
+    for _ in 0..10 {
+        dst.clear();
+        look_and_say(dst, src);
+        std::mem::swap(dst, src);
+    }
+    let part2 = src.len();
+
+    (part1.to_string(), part2.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use common::solution;
+
+    // Part 1
+    #[test]
+    fn p1_example_1() {
+        assert_eq!(look_and_say(&mut String::new(), "1"), "11");
+    }
+    #[test]
+    fn p1_example_2() {
+        assert_eq!(look_and_say(&mut String::new(), "11"), "21");
+    }
+    #[test]
+    fn p1_example_3() {
+        assert_eq!(look_and_say(&mut String::new(), "21"), "1211");
+    }
+    #[test]
+    fn p1_example_4() {
+        assert_eq!(look_and_say(&mut String::new(), "1211"), "111221");
+    }
+    #[test]
+    fn p1_example_5() {
+        assert_eq!(look_and_say(&mut String::new(), "111221"), "312211");
+    }
+    solution!(p1, p1_solution, "492982", ignore = "takes too long to run");
+
+    // Part 2
+    solution!(p2, p2_solution, "6989950", ignore = "takes too long to run");
+}
--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod day1;
+pub mod day10;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -18,4 +19,5 @@ pub const SOLUTIONS: &[common::Solution] = &[
     day7::SOLUTION,
     day8::SOLUTION,
     day9::SOLUTION,
+    day10::SOLUTION,
 ];
