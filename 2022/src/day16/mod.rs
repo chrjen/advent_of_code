5da3e7ab973e970172f9a71fd25fc1f2cbb02bd7
Christer Jensen <chr.code@gmail.com>
2023-01-02 02:05:41 +0100

    Add documentation to solution for 2022 - Day 16

--- 2022/src/day16/mod.rs
+++ 2022/src/day16/mod.rs
@@ -16,21 +16,35 @@ pub const SOLUTION: common::Solution = common::Solution {
 };
 
 const START: &str = "AA";
+const TIME_OPEN_VALVE: i32 = 1;
+const TIME_BETWEEN_NODES: i32 = 1;
 
+/// Takes a start node, a set of other nodes to use, time left not to exceed and
+/// a graph with weights between all the nodes. Returns the max pressure possible
+/// to release with the given input.
+///
+/// Uses Dynamic Programming to reduce the problem into sub-problems,
+/// recursively calling itself with one fewer nodes. With the nodes A, B, C and
+/// `A` as the start node, the first call to this function would be `(A, {B, C})`.
+/// This function would then recursively call itself with each node in `others`
+/// as the start node, removing itself from the set `others`, e.i. `(B, {C})`
+/// and `(C, {B})`. Lastly each of those call `(C, {})` and `(B, {})`,
+/// respectively. On each return only the biggest value is saved and added
+/// to the start node
 fn max_pressure(
     start: Node,
     others: &mut VecDeque<Node>,
     time_left: i32,
-    cg: &GraphMap<Node, i32, Undirected>,
+    graph: &GraphMap<Node, i32, Undirected>,
 ) -> i32 {
     let mut pressure = 0;
 
     for _ in 0..others.len() {
         let next = others.pop_front().unwrap();
 
-        let time_left = time_left - *cg.edge_weight(start, next).unwrap() - 1;
+        let time_left = time_left - *graph.edge_weight(start, next).unwrap() - TIME_OPEN_VALVE;
         if !time_left.is_negative() {
-            pressure = pressure.max(max_pressure(next, others, time_left, cg));
+            pressure = pressure.max(max_pressure(next, others, time_left, graph));
         }
 
         others.push_back(next);
@@ -53,7 +67,7 @@ pub fn solve(input: &[u8]) -> (String, String) {
     // Populate the graph with the parsed input.
     for (name, _, neighbours) in result.iter() {
         for &neighbour in neighbours.iter() {
-            graph.add_edge(nodes[name], nodes[neighbour], 1);
+            graph.add_edge(nodes[name], nodes[neighbour], TIME_BETWEEN_NODES);
         }
     }
 
@@ -103,6 +117,17 @@ pub fn solve(input: &[u8]) -> (String, String) {
     let part1 = max_pressure(start, &mut others.clone(), 30, &complete_graph);
 
     // Part 2.
+    // With two persons all nodes are partitioned in two sets and each person
+    // independently calculates `max_pressure` on only those nodes. The total
+    // pressure released is the sum of what the two persons released individually.
+    //
+    // A partition is represented as a binary number with bits showing which set
+    // a node belongs to. Example, with four nodes and the binary number `0110`
+    // then first node belongs to the elf, second and third to the elephant, and
+    // the fourth to the elf. Since we don't care about who gets each set, e.i.
+    // `0110` is the same as the bitwise inverse `1001`, we get 2^(n-1) total
+    // possible partitions for `n` nodes. We then iterate through all possible
+    // partitions taking the result from the partition with highest result.
     let num_partitions = 1 << (others.len() - 1);
     let part2 = (0..num_partitions)
         .into_par_iter()
